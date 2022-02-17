a4b2e14173a1eecff8c406fea98b154f19cdb543
Royce Cotcher <RoyCot@users.noreply.github.com>
2022-02-16 22:19:18 -0700

    Add project files.

---
+++ Models/EFJungleRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jungle.Models
+{
+    public class EFJungleRepository : IJungleRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFJungleRepository(BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IQueryable<Book> Books => context.Books;
+    }
+}
---
+++ Models/IJungleRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jungle.Models
+{
+    public interface IJungleRepository
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
